fc3fa4c9888e13d7932baf43c320c37bd16abaf5
Larry Hudson <10627494+larryhudson@users.noreply.github.com>
2023-11-20 07:40:53 +1100

    feat: initial commit

---
+++ src/pdf/index.ts
@@ -0,0 +1,33 @@
+import {
+  AzureKeyCredential,
+  DocumentAnalysisClient,
+} from "@azure/ai-form-recognizer";
+
+// this was in Microsoft's code sample and might be useful later
+function* getTextOfSpans(content, spans) {
+  for (const span of spans) {
+    yield content.slice(span.offset, span.offset + span.length);
+  }
+}
+
+export async function handlePdf(formFile) {
+  const AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT = import.meta.env
+    .AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT;
+  const AZURE_DOCUMENT_INTELLIGENCE_KEY = import.meta.env
+    .AZURE_DOCUMENT_INTELLIGENCE_KEY;
+  // create your `DocumentAnalysisClient` instance and `AzureKeyCredential` variable
+  const client = new DocumentAnalysisClient(
+    AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT,
+    new AzureKeyCredential(AZURE_DOCUMENT_INTELLIGENCE_KEY),
+  );
+
+  const fileArrayBuffer = await formFile.arrayBuffer();
+  const poller = await client.beginAnalyzeDocument(
+    "prebuilt-read",
+    fileArrayBuffer,
+  );
+
+  const result = await poller.pollUntilDone();
+
+  return result;
+}
